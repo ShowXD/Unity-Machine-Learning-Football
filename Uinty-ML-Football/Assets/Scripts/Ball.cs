f4144fc28e24030fb3832f70a56bc36cae898d52
ShowXD <n16418113@gmail.com>
2020-04-02 01:23:22 +0800

    Complete the 3/26 video

---
+++ Uinty-ML-Football/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    /// <summary>
+    /// 足球是否進入球門
+    /// </summary>
+    public static bool complete;
+
+    /// <summary>
+    /// 觸發開始事件
+    /// </summary>
+    /// <param name="other">碰到的物件碰撞資訊</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // 如果碰到物件的名稱等於"Sensor"
+        if (other.name == "Sensor")
+        {
+            // 進入球門
+            complete = true;
+        }
+    }
+}
